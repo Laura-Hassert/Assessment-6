2b292e0b4e841f7c7a762ad48956214dc27b956d
Laura-Hassert <laura.hassert@gmail.com>
2021-10-08 12:37:36 -0400

    completed Trello, automated tests, and deployment sketch

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -18,3 +18,41 @@ test('I can start a game', async () => {
     await button.click();
     
 });
+
+test('Clicking upper left box adds an X', async () => {
+
+    let upperLeft = await (await driver).findElement(By.id('cell-0'));
+    await upperLeft.click();
+    await driver.sleep(2000);
+    expect(await upperLeft.getText()).toContain('X')
+    
+});
+
+test('Clicking upper right box adds an X', async () => {
+
+    let upperRight = await (await driver).findElement(By.id('cell-2'));
+    await upperRight.click();
+    await driver.sleep(2000);
+    expect(await upperRight.getText()).toContain('X')
+    
+});
+
+test('Clicking lower right box adds an X', async () => {
+
+    let lowerRight = await (await driver).findElement(By.id('cell-8'));
+    await lowerRight.click();
+    await driver.sleep(2000);
+    expect(await lowerRight.getText()).toContain('X')
+    
+});
+
+// TESTING THE TEST - EXPECT A FAIL FOR THIS:
+// test('Clicking lower right box adds an O', async () => {
+
+//     let lowerRight = await (await driver).findElement(By.id('cell-8'));
+//     await lowerRight.click();
+//     await driver.sleep(2000);
+//     expect(await lowerRight.getText()).toContain('O')
+    
+// });
+
